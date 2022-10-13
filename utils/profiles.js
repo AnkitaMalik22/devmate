a6a5f155b4fc6be33303a05ef949d361f9c0e608
urlunaticguy <85105608+urlunaticguy@users.noreply.github.com>
2022-10-13 07:56:47 +0530

    adding my profile souvik

--- utils/profiles.js
+++ utils/profiles.js
@@ -1,86 +1,95 @@
 const profiles = [
-    {
-        name: "Ankita Malik",
-        role: "Student",
-        bio: "Aspiring Full Stack Web Developer",
-        skills: ["react", "html", "css", "javascript"],
-        github: "AnkitaMalik22",
-        twitter: "ankiitamalik",
-        instagram: "ankiitamalik"
-    },
-    {
-        name: "Thomas Qvidahl",
-        role: "Software Engineer",
-        bio: "Full Stack Developer",
-        skills: ["react", "html", "css", "kotlin"],
-        github: "tplive",
-        twitter: "qvidahl",
-        instagram: "tplive"
-    },
+  {
+    name: "Ankita Malik",
+    role: "Student",
+    bio: "Aspiring Full Stack Web Developer",
+    skills: ["react", "html", "css", "javascript"],
+    github: "AnkitaMalik22",
+    twitter: "ankiitamalik",
+    instagram: "ankiitamalik",
+  },
+  {
+    name: "Thomas Qvidahl",
+    role: "Software Engineer",
+    bio: "Full Stack Developer",
+    skills: ["react", "html", "css", "kotlin"],
+    github: "tplive",
+    twitter: "qvidahl",
+    instagram: "tplive",
+  },
 
-    {
-        name: "Sarthak Rajput",
-        role: "Student",
-        bio: "Aspiring Full Stack Developer",
-        skills: ["react", "html", "css", "Javascript"],
-        github: "rsarthak",
-        instagram: "sarthak_raj_put"
-    },
+  {
+    name: "Sarthak Rajput",
+    role: "Student",
+    bio: "Aspiring Full Stack Developer",
+    skills: ["react", "html", "css", "Javascript"],
+    github: "rsarthak",
+    instagram: "sarthak_raj_put",
+  },
 
-      {
-        name: "Andrew McDonald",
-        role: "Educational Consultant",
-        bio: "Educational Concultant",
-        skills: ["Scratch", "html", "python"],
-        github: "afmcdnL",
-        twitter: "afmcdnL",
-        instagram: "afmcdnL"
-    },
+  {
+    name: "Andrew McDonald",
+    role: "Educational Consultant",
+    bio: "Educational Concultant",
+    skills: ["Scratch", "html", "python"],
+    github: "afmcdnL",
+    twitter: "afmcdnL",
+    instagram: "afmcdnL",
+  },
 
+  {
+    name: "Roshan Kc",
+    role: "Student",
+    bio: "Full Stack Developer",
+    skills: [
+      "Python",
+      "Django",
+      "React",
+      "NextJs",
+      "AWS",
+      "Security Researcher",
+    ],
+    github: "Roshankc682",
+    twitter: "roshan_kc682",
+    instagram: "N/A",
+  },
 
-    {
-        name: "Roshan Kc",
-        role: "Student",
-        bio: "Full Stack Developer",
-        skills: ["Python", "Django", "React","NextJs","AWS","Security Researcher"],
-        github: "Roshankc682",
-        twitter: "roshan_kc682",
-        instagram: "N/A"
-    },
+  {
+    name: "Arun Jangra",
+    role: "Student",
+    bio: "Full Stack Web3 Developer",
+    skills: ["react", "nodejs", "express", "typescript", "solidity", "web3.js"],
+    github: "Arun89-crypto",
+    twitter: "w3Ts0ck3T",
+    instagram: "yopiter._.kick",
+  },
 
-    {
-        name: "Arun Jangra",
-        role: "Student",
-        bio: "Full Stack Web3 Developer",
-        skills: ["react", "nodejs", "express", "typescript", "solidity", "web3.js"],
-        github: "Arun89-crypto",
-        twitter: "w3Ts0ck3T",
-        instagram: "yopiter._.kick"
-    },
+  {
+    name: "Tirth Patel",
+    role: "Student",
+    bio: "Flutter Developer",
+    skills: ["Flutter", "Dart"],
+    github: "tirthkp",
+    twitter: "tirthkp",
+    instagram: "tirthkp",
+  },
 
+  {
+    name: "Aditi Chauhan",
+    role: "Student",
+    bio: "Aspiring Full Stack Developer",
+    skills: ["react", "html", "css", "javascript", "python"],
+    github: "Aditi-1709",
+    twitter: "don't have",
+    instagram: "don't have ",
+  },
 
-    {
-        name: "Tirth Patel",
-        role: "Student",
-        bio: "Flutter Developer",
-        skills: ["Flutter", "Dart"],
-        github: "tirthkp",
-        twitter: "tirthkp",
-        instagram: "tirthkp"
-      },
-
-    {
-        name: "Aditi Chauhan",
-        role: "Student",
-        bio: "Aspiring Full Stack Developer",
-        skills: ["react", "html", "css", "javascript" , "python" ],
-        github: "Aditi-1709",
-        twitter: "don't have",
-        instagram: "don't have "
-
-    },
-
-
-]
+  {
+    name: "Souvik Das",
+    role: "Student",
+    bio: "Aspiring Developer - App and Web",
+    skills: ["react", "html", "css", "javascript", "python", "Swift"],
+  },
+];
 
 export default profiles;
