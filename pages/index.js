1b702edb113593be191c94cd8ae95d21515b427f
Shinchan3102 <106232138+Shinchan3102@users.noreply.github.com>
2022-10-16 12:39:49 +0530

    makes the cross under search button functional

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,7 @@ export default function Home() {
                     <div className="flex justify-center items-center m-1 font-medium py-1 px-2 bg-white rounded-full text-teal-700  border border-teal-300 ">
                       <div className="text-xs font-normal leading-none max-w-full flex-initial">{lang}</div>
                       <div className="flex flex-auto flex-row-reverse">
-                        <div>
+                        <div onClick={()=>{setLang()}}>
                           <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x cursor-pointer hover:text-teal-400 rounded-full w-4 h-4 ml-2">
                             <line x1="18" y1="6" x2="6" y2="18"></line>
                             <line x1="6" y1="6" x2="18" y2="18"></line>
