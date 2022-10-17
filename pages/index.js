9abe630c77fac7a34440183abd034186f186b860
Killua Zoldyck <killua@DESKTOP-UM5BUOK.localdomain>
2022-10-17 13:23:34 +0100

    FEAT: lang are stackable now.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useState, useEffect } from 'react';
+import {useRef, useState, useEffect } from 'react';
 import SKILLS from '../utils/skills';
 import { ProfileCard } from '../components/ProfileCard';
 import profiles from '../utils/profiles';
@@ -13,7 +13,8 @@ import Typewriter from 'typewriter-effect';
 
 export default function Home() {
   const [display, setDisplay] = useState("absolute");
-  const [lang, setLang] = useState();
+  const [tags , setTags] = useState([]);
+  const [lang, setLang] = useState([]);
   // let languages=new Set();
   // let langs=Array.from(languages)
 
@@ -84,8 +85,12 @@ export default function Home() {
                   <div className="flex flex-auto flex-wrap">
 
 
-                    <div className="flex justify-center items-center m-1 font-medium py-1 px-2 bg-white rounded-full text-teal-700  border border-teal-300 ">
-                      <div className="text-xs font-normal leading-none max-w-full flex-initial">{lang}</div>
+             
+                      {lang.map((item)=>{return ( <>
+                        <div className="flex justify-center items-center m-1 font-medium py-1 px-2 bg-white rounded-full text-teal-700  border border-teal-300 ">
+                      <div  className="text-xs font-normal leading-none max-w-full flex-initial">{item}</div>
+
+
                       <div className="flex flex-auto flex-row-reverse">
                         <div onClick={()=>{setLang()}}>
                           <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x cursor-pointer hover:text-teal-400 rounded-full w-4 h-4 ml-2">
@@ -94,7 +99,10 @@ export default function Home() {
                           </svg>
                         </div>
                       </div>
-                    </div>
+                      </div>
+
+                      </> )})}
+                
 
                     <div className="flex-1">
                       <input placeholder="" className="bg-transparent p-1 px-2 appearance-none outline-none h-full w-full text-gray-800" />
@@ -110,13 +118,13 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-              <div className={`${display === "hidden" ? "absolute" : "hidden"} shadow top-100 bg-white z-40 w-full lef-0 rounded max-h-select overflow-y-auto svelte-5uyqqj`}>
+              <div  className={`${display === "hidden" ? "absolute" : "hidden"} shadow top-100 bg-white z-40 w-full lef-0 rounded max-h-select overflow-y-auto svelte-5uyqqj`}>
                 <div className="flex flex-col w-full">
 
                   {
                     SKILLS && SKILLS.map((skill, index) => {
                       return (
-                        <div className="cursor-pointer w-full border-gray-100 rounded-t border-b hover:" onClick={() => { setLang(skill.name) }} key={index}>
+                        <div className="cursor-pointer w-full border-gray-100 rounded-t border-b hover:" onClick={() => { setLang([...lang,skill.name]) }} key={index}>
                           <div className="flex w-full items-center p-2 pl-2 border-transparent border-l-2 relative hover:border-teal-100">
                             <div className="w-full items-center flex">
                               <div className="mx-2 leading-6" data-skill={skill.name}>{skill.name}</div>
