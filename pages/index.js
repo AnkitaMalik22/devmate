5d97511a0e11edeac7de45ef0fda50633ec4c24d
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2022-10-25 20:24:23 +0530

    no need to add the alert here
    Signed-off-by: Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -147,13 +147,6 @@ export default function Home() {
                 loop: true,
               }}
             />
-            <Typewriter
-              options={{
-                strings: ["Scroll down to see your results..."],
-                autoStart: true,
-                loop: true,
-              }}
-            />
           </code>
         </p>
 
